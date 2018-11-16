305e67018b121617aa4daef4808abb56e8e70a11
sbsends <sbsends@gmail.com>
2018-11-15 23:58:39 -0600

    all: added files to repo

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * gcloud functions deploy cloudBuildBadge --runtime nodejs6 --trigger-resource cloud-builds --trigger-event google.pubsub.topic.publish
+ * @param {object} event The Cloud Functions event.
+ * @param {function} callback The callback function.
+ */
+const { Storage } = require('@google-cloud/storage');
+
+exports.cloudBuildBadge = (event, callback) => {
+  const pubsubMessage = event.data;
+  if  (pubsubMessage.data) {
+    buildResource = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
+    repo = buildResource.source.repoSource.repoName === "github-tensortask-go";
+    branch = buildResource.source.repoSource.branchName === "master";
+    status = buildResource.status;
+
+    const storage = new Storage();
+    if (repo && branch && status == "SUCCESS") {
+      storage.bucket("tensortask-static")
+      .file("build/success.svg")
+      .copy(storage.bucket("tensortask-static")
+      .file("build/badge.svg"));
+    }
+    if (repo && branch && status == "FAILURE") {
+      storage.bucket("tensortask-static")
+      .file("build/failure.svg")
+      .copy(storage.bucket("tensortask-static")
+      .file("build/badge.svg"));
+    }
+  }
+  callback();
+};
+
+
