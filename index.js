e36340e55faed11a033b53fcb16843d437122208
sbsends <sbsends@gmail.com>
2018-11-26 20:53:22 -0600

    index: fixed template and checks for data

--- index.js
+++ index.js
@@ -9,22 +9,24 @@ exports.${badge} = (event, callback) => {
   const pubsubMessage = event.data;
   if  (pubsubMessage.data) {
     buildResource = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
-    repo = buildResource.source.repoSource.repoName === "${repo}";
-    branch = buildResource.source.repoSource.branchName === "${branch}";
-    status = buildResource.status;
+    if (buildResource.source.repoSource.repoName && buildResource.source.repoSource.branchName && buildResource.status) {
+      repo = buildResource.source.repoSource.repoName === "${repo}";
+      branch = buildResource.source.repoSource.branchName === "${branch}";
+      status = buildResource.status;
 
-    const storage = new Storage();
-    if (repo && branch && status == "SUCCESS") {
-      storage.bucket("${bucket}")
-      .file("build/success.svg")
-      .copy(storage.bucket("tensortask-static")
-      .file("build/${badge}.svg"));
-    }
-    if (repo && branch && status == "FAILURE") {
-      storage.bucket("${bucket}")
-      .file("build/failure.svg")
-      .copy(storage.bucket("tensortask-static")
-      .file("build/${badge}.svg"));
+      const storage = new Storage();
+      if (repo && branch && status == "SUCCESS") {
+        storage.bucket("${bucket}")
+        .file("build/success.svg")
+        .copy(storage.bucket("${bucket}")
+        .file("build/${badge}.svg"));
+      }
+      if (repo && branch && status == "FAILURE") {
+        storage.bucket("${bucket}")
+        .file("build/failure.svg")
+        .copy(storage.bucket("${bucket}")
+        .file("build/${badge}.svg"));
+      }
     }
   }
   callback();
