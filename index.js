2950f400ce6495b5737ab52ee1f79c2c12a93762
sbsends <sbsends@gmail.com>
2018-12-21 00:07:44 -0600

    index: added additional check

--- index.js
+++ index.js
@@ -7,28 +7,36 @@ const { Storage } = require('@google-cloud/storage');
 
 exports.${badge} = (event, callback) => {
   const pubsubMessage = event.data;
-  if  (pubsubMessage.data) {
+  if (pubsubMessage.data) {
     buildResource = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
-    if (buildResource.source.repoSource.repoName && buildResource.source.repoSource.branchName && buildResource.status) {
-      repo = buildResource.source.repoSource.repoName === "${repo}";
-      branch = buildResource.source.repoSource.branchName === "${branch}";
+    if (buildResource.source) {
+      if (buildResource.source.repoSource.repoName && buildResource.source.repoSource.branchName) {
+        repo = buildResource.source.repoSource.repoName === "${repo}";
+        branch = buildResource.source.repoSource.branchName === "${branch}";
+      }
+    } else {
+      callback();
+    }
+    if (buildResource.status) {
       status = buildResource.status;
+    } else {
+      callback();
+    }
 
-      const storage = new Storage();
-      if (repo && branch && status == "SUCCESS") {
-        storage.bucket("${bucket}")
+    const storage = new Storage();
+    if (repo && branch && status == "SUCCESS") {
+      storage.bucket("${bucket}")
         .file("build/success.svg")
         .copy(storage.bucket("${bucket}")
-        .file("build/${badge}.svg"));
-        console.log("switched badge to build success")
-      }
-      if (repo && branch && status == "FAILURE") {
-        storage.bucket("${bucket}")
+          .file("build/${badge}.svg"));
+      console.log("switched badge to build success")
+    }
+    if (repo && branch && status == "FAILURE") {
+      storage.bucket("${bucket}")
         .file("build/failure.svg")
         .copy(storage.bucket("${bucket}")
-        .file("build/${badge}.svg"));
-        console.log("switched badge to build failure")
-      }
+          .file("build/${badge}.svg"));
+      console.log("switched badge to build failure")
     }
   }
   callback();
